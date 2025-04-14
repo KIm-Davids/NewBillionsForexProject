ea1503de8e02952cdd2478dbb24c11342080102b
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-14 05:17:45 +0100

    Fixing the retrieving of data from the db

--- pages/UserContext.js
+++ pages/UserContext.js
@@ -1,18 +1,18 @@
-import React, { createContext, useState, useContext } from 'react';
-
-// Create the context for user data (email in this case)
-const UserContext = createContext();
-
-// Custom hook to access the context data
-export const useUser = () => useContext(UserContext);
-
-// Provider component that wraps the app and provides the global state
-export const UserProvider = ({ children }) => {
-    const [email, setEmail] = useState('');  // Email state
-
-    return (
-        <UserContext.Provider value={{ email, setEmail }}>
-            {children}
-        </UserContext.Provider>
-    );
-};
+// import React, { createContext, useState, useContext } from 'react';
+//
+// // Create the context for user data (email in this case)
+// const UserContext = createContext();
+//
+// // Custom hook to access the context data
+// export const useUser = () => useContext(UserContext);
+//
+// // Provider component that wraps the app and provides the global state
+// export const UserProvider = ({ children }) => {
+//     const [email, setEmail] = useState('');  // Email state
+//
+//     return (
+//         <UserContext.Provider value={{ email, setEmail }}>
+//             {children}
+//         </UserContext.Provider>
+//     );
+// };
