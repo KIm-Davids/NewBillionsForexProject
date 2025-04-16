e78b0ada29da39cb4b32fdbec5bb14879c2eb377
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-16 15:54:41 +0100

    Working on the user dashboard

--- src/app/customer-dashboard/page.jsx
+++ src/app/customer-dashboard/page.jsx
@@ -549,6 +549,7 @@
                                                       packageType: packageType,
                                                   }),
                                               });
+                                              setResponseMessage("Transaction Processing ...")
 
                                               if (response.ok) {
 
