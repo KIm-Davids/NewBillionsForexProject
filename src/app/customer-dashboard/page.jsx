8b9b1b63d78b81f78fa0ea3f1dcfb2b4038d4896
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-19 15:31:12 +0100

    Working on the users daily profits

--- src/app/customer-dashboard/page.jsx
+++ src/app/customer-dashboard/page.jsx
@@ -260,7 +260,7 @@
 
                                           try {
                                               const userEmail = localStorage.getItem("userEmail");
-                                              const response = await fetch('https://billions-backend-1.onrender.com/getDailyProfit', {
+                                              const response = await fetch('https://billions-backend-1.onrender.com/getNetProfit', {
                                                   method: 'POST',
                                                   headers: {
                                                       'Content-Type': 'application/json',
@@ -280,7 +280,6 @@
                                                   } else {
                                                       setProfits(0); // Default to 0 if no profit found
                                                   }
-
                                                   setLoading(false);
                                               } else {
                                                   setLoading(false);
