07e8d43b259d28dffecba6c41eecf883236f5929
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-25 19:58:36 +0100

    Worked on the referral bonus

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   experimental: {
-//     appDir: true,
-//   },
-// };
-//
-// module.exports = nextConfig;
-
-
-// next.config.js
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/404',
-        permanent: false, // This makes the redirect temporary
-      },
-    ];
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  experimental: {
+    appDir: true,
   },
 };
+
+module.exports = nextConfig;
+
+
+// // next.config.js
+// module.exports = {
+//   async redirects() {
+//     return [
+//       {
+//         source: '/:path*',
+//         destination: '/404',
+//         permanent: false, // This makes the redirect temporary
+//       },
+//     ];
+//   },
+// };
