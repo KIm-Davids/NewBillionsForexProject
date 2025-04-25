c767f2eedf72ddac25ceeb9c83685d1d132f02e7
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-25 16:22:25 +0100

    Worked on the referral bonus

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
-import "../styles/globals.css";
-import { ThemeProvider } from "../src/components/theme-provider"; // adjust path if needed
-import { UserProvider } from "../src/context/UserContext"; // add this import
+// import "../styles/globals.css";
+// import { ThemeProvider } from "../src/components/theme-provider"; // adjust path if needed
+// import { UserProvider } from "../src/context/UserContext"; // add this import
+//
+// export default function App({ Component, pageProps }) {
+//     return (
+//         <UserProvider> {/* Wrap everything with UserProvider */}
+//             <ThemeProvider>
+//                 <Component {...pageProps} />
+//             </ThemeProvider>
+//         </UserProvider>
+//     );
+// }
 
-export default function App({ Component, pageProps }) {
-    return (
-        <UserProvider> {/* Wrap everything with UserProvider */}
-            <ThemeProvider>
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </UserProvider>
-    );
+
+// pages/_app.js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        // Redirect all routes to /404
+        router.replace('/404');
+    }, [router]);
+
+    return <Component {...pageProps} />;
 }
+
+export default MyApp;
